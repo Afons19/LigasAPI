2010a0913db04b0c572cf7282f652bae2ba1a79f
Afonso Aurélio <Afons19@hotmail.com>
2026-07-08 12:38:07 +0100

    Actualização da API

--- frontend/src/services/api.js
+++ frontend/src/services/api.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const api = axios.create({
-    baseURL: 'https://ligasapi.onrender.com/api/'
+    baseURL: 'https://ligas-api.onrender.com/api'
 });
 
 export default api;
